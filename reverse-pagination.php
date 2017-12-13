73c8c380bdac53cc25ffa5ecf56da8acbcd53136
Russell Heimlich <kingkool68@gmail.com>
2017-12-12 23:19:35 -0500

    Linting.

--- reverse-pagination.php
+++ reverse-pagination.php
@@ -39,25 +39,24 @@ class WP_Reverse_Pagination {
 	// This will help in figuring out how to make this work https://ozthegreat.io/wordpress/wordpress-database-queries-speed-sql_calc_found_rows
 	public function action_pre_get_posts( $wp_query ) {
 		global $wpdb;
-		if( is_admin() ) {
+		if ( is_admin() ) {
 			return $wp_query;
 		}
 
 		$q = $wp_query->query_vars;
 
-		if( $wp_query->is_main_query() ) {
+		if ( $wp_query->is_main_query() ) {
 
 			$wp_query->query_vars['no_found_rows'] = 1;
 
-
 			$order = strtoupper( $q['order'] );
-			if(
+			if (
 				empty( $q['order'] ) ||
 				( $order != 'DESC' && $order != 'ASC' )
 			) {
 				$q['order'] = 'ASC';
 			}
-			if( !$q['paged'] ) {
+			if ( ! $q['paged'] ) {
 				// $q['order'] = 'DESC';
 			}
 
@@ -67,20 +66,20 @@ class WP_Reverse_Pagination {
 	}
 
 	public function action_wp_head() {
-		if( is_main_query() ) {
+		if ( is_main_query() ) {
 			set_query_var( 'paged', $this->new_paged );
 		}
 	}
 
-	public function filter_redirect_canonical($redirect_url, $requested_url) {
+	public function filter_redirect_canonical( $redirect_url, $requested_url ) {
 		global $paged;
-		if( is_admin() ) {
+		if ( is_admin() ) {
 			return;
 		}
 
 		$paged = $this->new_paged;
 
-		if( stristr( $requested_url, '/page/1/' ) ) {
+		if ( stristr( $requested_url, '/page/1/' ) ) {
 			return $requested_url;
 		}
 
@@ -91,19 +90,19 @@ class WP_Reverse_Pagination {
 		global $paged, $wp_query, $wp_rewrite;
 
 		$old_paged = $wp_query->max_num_pages - $paged;
-		if( $old_paged == 1 ) {
+		if ( $old_paged == 1 ) {
 			// $paged = 1;
 		}
 
 		$old_next_page = $paged + 1;
 		// It's a next page...
-		if( stripos( $result, $wp_rewrite->pagination_base . '/' . $old_next_page ) || $paged == 1 ) {
+		if ( stripos( $result, $wp_rewrite->pagination_base . '/' . $old_next_page ) || $paged == 1 ) {
 			$new_next_page = $paged - 1;
-			if( $paged == 1 ) {
+			if ( $paged == 1 ) {
 				$new_next_page = $wp_query->max_num_pages - 1;
 			}
 
-			if( $old_paged == 1 ) {
+			if ( $old_paged == 1 ) {
 				$new_next_page = 1;
 			}
 
@@ -111,20 +110,20 @@ class WP_Reverse_Pagination {
 		}
 
 		$old_prev_page = $paged - 1;
-		if( $old_prev_page == 0 ) {
+		if ( $old_prev_page == 0 ) {
 			return $result;
 		}
-		if( $old_prev_page == 1 ) {
+		if ( $old_prev_page == 1 ) {
 			return $result . $wp_rewrite->pagination_base . '/' . ($paged + 1) . '/';
 		}
 
 		// It's a previous page...
-		if( stripos( $result, $wp_rewrite->pagination_base . '/' . $old_prev_page ) ) {
+		if ( stripos( $result, $wp_rewrite->pagination_base . '/' . $old_prev_page ) ) {
 			$new_prev_page = $paged + 1;
-			if( $new_prev_page == $wp_query->max_num_pages ) {
+			if ( $new_prev_page == $wp_query->max_num_pages ) {
 				$new_prev_page = 2;
 			}
-			return str_replace( $wp_rewrite->pagination_base . '/' . $old_prev_page, $wp_rewrite->pagination_base . '/' . $new_prev_page , $result);
+			return str_replace( $wp_rewrite->pagination_base . '/' . $old_prev_page, $wp_rewrite->pagination_base . '/' . $new_prev_page , $result );
 		}
 
 		return $result;
@@ -136,7 +135,7 @@ class WP_Reverse_Pagination {
 		$original_sql = $sql;
 		$q = $wp_query->query_vars;
 
-		if( $wp_query->is_main_query() ) {
+		if ( $wp_query->is_main_query() ) {
 
 			$pieces = explode( 'FROM', $sql );
 			$max_pages_sql = 'SELECT COUNT(*) FROM' . $pieces[1];
@@ -148,7 +147,7 @@ class WP_Reverse_Pagination {
 			$wp_query->found_posts = apply_filters_ref_array( 'found_posts', array( $wp_query->found_posts, &$wp_query ) );
 
 			$posts_per_page = intval( $wp_query->query_vars['posts_per_page'] );
-			if( !$posts_per_page ) {
+			if ( ! $posts_per_page ) {
 				$posts_per_page = get_option( 'posts_per_page' );
 			}
 			$wp_query->max_num_pages = ceil( $wp_query->found_posts / $posts_per_page );
@@ -164,7 +163,7 @@ class WP_Reverse_Pagination {
 			$old_offset = ( $paged * $posts_per_page) - $posts_per_page;
 
 			$find = 'LIMIT ' . $old_offset . ', ' . $posts_per_page;
-			$replace =  'LIMIT ' . $new_offset .  ', ' . $posts_per_page;
+			$replace =  'LIMIT ' . $new_offset . ', ' . $posts_per_page;
 			$sql = str_replace( $find, $replace, $sql );
 			/*
 			echo '<pre>';
